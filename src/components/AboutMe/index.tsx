d16211b5c7552ea809797b241632b1bc5cdb7cf0
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-23 22:48:43 +0530

    Add about me

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import './style.css';
 
-export const AddLink: React.FC = () =>{
+export const AboutMe: React.FC = () =>{
     return(
-           <div>
-               about me
+           <div className="about-content">
+               <p>
+                   Hi,
+                   <br />
+                   I am a Btech CSE graduate(2020 batch).
+                   <br />
+                   I am an intermediate level data structures and algorithms programmer, actively seeking opportunites to work as
+                   a Software Engineer.I love to code things and make applications from scratch to deployable state keeping in mind not only
+                   functionality but also quality in writing code.
+
+               </p>
+               
            </div>
     );
 }
--- src/components/Details/index.tsx
+++ src/components/Details/index.tsx
@@ -2,39 +2,74 @@ import React from "react";
 import "./style.css";
 import "antd/dist/antd.css";
 
-import { Tabs } from 'antd';
+import { Tabs } from "antd";
 import { Skills } from "../Skills";
+import { AboutMe } from "../AboutMe";
 export const Details: React.FC = () => {
-  
   const { TabPane } = Tabs;
- 
+
   return (
     <div className="detail-nav">
-         <Tabs defaultActiveKey="1" size={"large"} style={{padding:10, backgroundColor:"black",color:"white",height:"100%"}}>
-          <TabPane tab="About Me" key="1" style={{backgroundColor:"white", height:"76vh",width:""}}>
-            Content of tab 1
-          </TabPane>
-          <TabPane tab="Work Experience" key="2" style={{backgroundColor:"white", height:"76vh"}}>
-            Content of tab 2
-          </TabPane>
-          <TabPane tab="Skills" key="3" style={{backgroundColor:"white", height:"76vh",color:"black"}}>
-            <Skills />
-          </TabPane>
-          <TabPane tab="Projects" key="4" style={{backgroundColor:"white", height:"76vh"}}>
-            Content of tab 3
-          </TabPane>
-          <TabPane tab="Sport Programming" key="5" style={{backgroundColor:"white", height:"76vh"}}>
-            Content of tab 3
-          </TabPane>
-          <TabPane tab="Achievements" key="6" style={{backgroundColor:"white", height:"76vh"}}>
-            Content of tab 3
-          </TabPane>
-          <TabPane tab="Resume" key="7" style={{backgroundColor:"white", height:"76vh"}}>
-            Content of tab 3
-          </TabPane>
-        </Tabs>
-
-      
+      <Tabs
+        defaultActiveKey="1"
+        size={"large"}
+        style={{
+          padding: 10,
+          backgroundColor: "black",
+          color: "white",
+          height: "100%",
+        }}
+      >
+        <TabPane
+          tab="About Me"
+          key="1"
+          style={{ backgroundColor: "white", height: "76vh", width: "" }}
+        >
+          <AboutMe />
+        </TabPane>
+        <TabPane
+          tab="Work Experience"
+          key="2"
+          style={{ backgroundColor: "white", height: "76vh" }}
+        >
+          Content of tab 2
+        </TabPane>
+        <TabPane
+          tab="Skills"
+          key="3"
+          style={{ backgroundColor: "white", height: "76vh", color: "black" }}
+        >
+          <Skills />
+        </TabPane>
+        <TabPane
+          tab="Projects"
+          key="4"
+          style={{ backgroundColor: "white", height: "76vh" }}
+        >
+          Content of tab 3
+        </TabPane>
+        <TabPane
+          tab="Sport Programming"
+          key="5"
+          style={{ backgroundColor: "white", height: "76vh" }}
+        >
+          Content of tab 3
+        </TabPane>
+        <TabPane
+          tab="Achievements"
+          key="6"
+          style={{ backgroundColor: "white", height: "76vh" }}
+        >
+          Content of tab 3
+        </TabPane>
+        <TabPane
+          tab="Resume"
+          key="7"
+          style={{ backgroundColor: "white", height: "76vh" }}
+        >
+          Content of tab 3
+        </TabPane>
+      </Tabs>
     </div>
   );
 };
