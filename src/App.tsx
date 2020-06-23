4589651a1cfeead96782f2429a33e70281d6f482
Shorya Kaushik <shorya.kaushik@hashedin.com>
2020-06-23 15:40:14 +0530

    Create Component Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-const App = () =>{
+const App: React.FC  = () =>{
   return (
     <div className="App">
       Hello
---
+++ src/components/Resume/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export const AddLink: React.FC = () =>{
+    return(
+           <div>
+               about me
+           </div>
+    );
+}
